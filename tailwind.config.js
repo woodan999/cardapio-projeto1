ebf17b4d45b93cda744086beb37c1748af87f0ba
carllos daniel <keret@2034623987-58485353-2442511837.2034623987-58485353-2442511837>
2026-02-26 23:16:05 -0300

    cardapio

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+   theme: {
+    extend: {
+      backgroundImage: {
+        "home": "url('/assets/bg.png')"
+      },
+    },
+  },
+
+  plugins: [],
+}
+  // ...
